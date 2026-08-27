d7d2edaadfaace4a08cd9f2b5f8974bf5dcf0f68
bewle <everbelle@proton.me>
2026-08-27 17:40:08 -0400

    feat: add anchor positioning unocss preset

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig, presetWind4, transformerDirectives, transformerVariantGroup } from 'unocss'
+import {
+  defineConfig,
+  presetWind4,
+  definePreset,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+const presetAnchorPositioning = definePreset(() => {
+  return {
+    name: 'unocss-anchor-positioning-preset',
+    rules: [
+      [
+        /(?<=position-anchor-)(?<name>[a-zA-Z0-9]+)/g,
+        ([name]) => ({
+          'position-anchor': `--${name}`,
+        }),
+      ],
+      [
+        /(?<=anchor-name-)(?<name>[a-zA-Z0-9]+)/g,
+        ([name]) => ({
+          'anchor-name': `--${name}`,
+        }),
+      ],
+      [
+        /(?<=anchor-)(?<pos>left|right|top|bottom|inset)/g,
+        ([pos]) => {
+          if (pos === 'inset') {
+            return {
+              bottom: 'anchor(bottom)',
+              left: 'anchor(left)',
+              right: 'anchor(right)',
+              top: 'anchor(top)',
+            }
+          }
+          return {
+            [pos]: `anchor(${pos})`,
+          }
+        },
+      ],
+    ],
+  }
+})
 
 export default defineConfig({
   presets: [
+    presetAnchorPositioning,
     presetWind4({
       preflights: {
         reset: true,
