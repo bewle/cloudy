38ff95a1a1971119ef92f87425b869e5fb9eaad0
bewle <everbelle@proton.me>
2026-08-28 15:55:22 -0400

    chore: edit eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: false,
   ignores: ['.data/**', '.nitro/**', '.nuxt/**', '.output/**', 'dist/**'],
+  imports: false,
   jsonc: false,
+  perfectionist: false,
   pnpm: false,
   stylistic: false,
   unocss: true,
