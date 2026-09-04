157152f3fc4acac2230643161988bded65378576
bewle <everbelle@proton.me>
2026-09-04 14:32:28 -0400

    chore: add input field placeholders constant

--- shared/constants/input.ts
+++ shared/constants/input.ts
@@ -1,2 +1,13 @@
 export const INPUT__OPTIONS = ['track', 'artist', 'playlist'] as const
 export type InputOption = (typeof INPUT__OPTIONS)[number]
+
+export const INPUT__FIELD_PLACEHOLDERS = [
+  'https://soundcloud.com/gunjump/komodo',
+  'https://soundcloud.com/2blacktoostrong/fraxinus-americana',
+  'https://soundcloud.com/yimellonline/girlhop',
+  'https://soundcloud.com/user-929794849/bed2',
+  'https://soundcloud.com/goodnightbed/whispering-window-part-8',
+  'https://soundcloud.com/ragzthewizard/penance',
+  'https://soundcloud.com/hav-lyfe/truliza4u',
+  'https://soundcloud.com/aerateloops/its-all-up',
+]
