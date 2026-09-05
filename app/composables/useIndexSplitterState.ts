4c36954070f62bb6e2d85de88eba738340f8dd0e
bewle <everbelle@proton.me>
2026-09-05 15:28:35 -0400

    fix: set default index splitter layout size

--- app/composables/useIndexSplitterState.ts
+++ app/composables/useIndexSplitterState.ts
@@ -1,1 +1,2 @@
-export const useIndexSplitterState = () => useCookie<number[]>('index-splitter-layout')
+export const useIndexSplitterState = () =>
+  useCookie<number[]>('index-splitter-layout', { default: () => [GENERAL__INDEX_DEFAULT_SIZE] })
