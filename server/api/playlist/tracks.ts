ee91f602b020b3a5a96cda42232d98cb23216f8d
bewle <everbelle@proton.me>
2026-09-08 14:44:39 -0400

    fix: correct error ode

--- server/api/playlist/tracks.ts
+++ server/api/playlist/tracks.ts
@@ -1,15 +1,11 @@
 const querySchema = v.object({
-  nextHref: v.optional(v.string()),
   url: v.optional(v.string()),
 })
 
 export default defineEventHandler(async (event): Promise<SCTrackSearchSummary> => {
   const query = validateQuery(event, querySchema)
 
-  if (query.nextHref)
-    return summarizeTrackSearch(await getUserTracksPage(assertScHref(query.nextHref)))
-
-  if (isNil(query.url)) throw validationErrors.NOT_ENOUGH_ARTIST_INFO()
+  if (isNil(query.url)) throw validationErrors.NOT_ENOUGH_PLAYLIST_INFO()
 
   const playlistMeta = await getPlaylistMeta(query.url)
   const search = await getPlaylistTracks(playlistMeta)
