d5dec363316b2ccaa9d62f5330fe8791c0c1a12b
bewle <everbelle@proton.me>
2026-09-08 16:36:17 -0400

    feat: add initial load skeletons

--- app/components/sidebar/root/Content.vue
+++ app/components/sidebar/root/Content.vue
@@ -15,11 +15,15 @@ const active = computed(() => (tab.value ? sourceMap[tab.value] : undefined))
 const tracks = computed(() => active.value?.tracks.value?.filter(isTrackSummary) ?? [])
 const canLoadMore = computed(() => active.value?.canLoadMore.value ?? false)
 const isLoading = computed(() => active.value?.isLoading.value ?? false)
+const isLoadingInitial = computed(() => isLoading.value && tracks.value.length === 0)
 
 const intersecting = ref(false)
 watch([intersecting, isLoading], ([hit, loading]) => {
   if (hit && !loading && canLoadMore.value) active.value?.loadNextHref()
 })
+
+const virtualizer = useTemplateRef('list')
+watch([artist, playlist], () => virtualizer.value?.rowVirtualizer.scrollToIndex(0))
 </script>
 
 <template>
@@ -35,19 +39,26 @@ watch([intersecting, isLoading], ([hit, loading]) => {
     <div class="flex-1 shrink size-full overflow-auto">
       <SidebarRootContentList
         v-if="tab"
+        ref="list"
         v-slot="{ rowVirtualizer }"
         :show-sentinel="canLoadMore"
         :list="tracks"
         item-key="id"
         @sentinel="intersecting = $event"
       >
-        <SidebarRootContentTabTrackCard
-          v-for="row in rowVirtualizer.getVirtualItems()"
-          :key="row.index"
-          class="w-full left-0 top-0 absolute"
-          :style="{ transform: `translateY(${row.start}px)` }"
-          :track="tracks[row.index]!"
-        />
+        <template v-if="!isLoadingInitial">
+          <SidebarRootContentTabTrackCard
+            v-for="row in rowVirtualizer.getVirtualItems()"
+            :key="row.index"
+            class="w-full left-0 top-0 absolute"
+            :style="{ transform: `translateY(${row.start}px)` }"
+            :track="tracks[row.index]!"
+          />
+        </template>
+
+        <template v-else>
+          <SidebarRootContentTabTrackCardSkeleton v-for="i in 6" :key="i" />
+        </template>
       </SidebarRootContentList>
     </div>
   </div>
---
+++ app/components/sidebar/root/content/tab/TrackCardSkeleton.vue
@@ -0,0 +1,5 @@
+<template>
+  <SidebarRootContentListCard class="border-none h-5.625rem p-0 mb-2 rounded">
+    <USkeleton class="size-full" />
+  </SidebarRootContentListCard>
+</template>
