c283689d7dc6edb0d68cd8f8dec80cbfd9fe2d64
bewle <everbelle@proton.me>
2026-09-08 14:05:28 -0400

    refactor: consolidate list impls

--- app/components/sidebar/root/Content.vue
+++ app/components/sidebar/root/Content.vue
@@ -1,15 +1,25 @@
-<script lang="ts">
-import { SidebarRootContentTabArtist, SidebarRootContentTabPlaylist } from '#components'
-const tabComponentMap = {
-  artist: SidebarRootContentTabArtist,
-  multitrack: SidebarRootContentTabArtist,
-  playlist: SidebarRootContentTabPlaylist,
-}
-</script>
-
 <script lang="ts" setup>
 const layout = useIndexSplitterState()
-const { tab } = useSidebarState()
+const { tab, artist, playlist } = useSidebarState()
+
+const artistSource = useArtistTracks(artist)
+const playlistSource = usePlaylistTracks(playlist)
+
+const sourceMap = {
+  artist: artistSource,
+  multitrack: artistSource,
+  playlist: playlistSource,
+} satisfies Record<SidebarTab, unknown>
+
+const active = computed(() => (tab.value ? sourceMap[tab.value] : undefined))
+const tracks = computed(() => active.value?.tracks.value?.filter(isTrackSummary) ?? [])
+const canLoadMore = computed(() => active.value?.canLoadMore.value ?? false)
+const isLoading = computed(() => active.value?.isLoading.value ?? false)
+
+const intersecting = ref(false)
+watch([intersecting, isLoading], ([hit, loading]) => {
+  if (hit && !loading && canLoadMore.value) active.value?.loadNextHref()
+})
 </script>
 
 <template>
@@ -23,7 +33,22 @@ const { tab } = useSidebarState()
     <SidebarRootContentSearch />
 
     <div class="flex-1 shrink size-full overflow-auto">
-      <component :is="tab && tabComponentMap[tab]" />
+      <SidebarRootContentList
+        v-if="tab"
+        v-slot="{ rowVirtualizer }"
+        :show-sentinel="canLoadMore"
+        :list="tracks"
+        item-key="id"
+        @sentinel="intersecting = $event"
+      >
+        <SidebarRootContentTabTrackCard
+          v-for="row in rowVirtualizer.getVirtualItems()"
+          :key="row.index"
+          class="w-full left-0 top-0 absolute"
+          :style="{ transform: `translateY(${row.start}px)` }"
+          :track="tracks[row.index]!"
+        />
+      </SidebarRootContentList>
     </div>
   </div>
 </template>
