0917838c37fa63ad47e8fd5dc083c1f3f60afa73
bewle <everbelle@proton.me>
2026-09-10 14:19:57 -0400

    feat: add content list card buttons component

---
+++ app/components/sidebar/root/content/list/CardButtons.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  trackRow: TrackRow
+}>()
+
+const { isBatchRunning } = useDownloads()
+
+const { multitrackList } = useSidebarState()
+const inMultitrackList = computed(() => multitrackList.has(props.trackRow.url))
+
+const addMultitrackItem = () => multitrackList.add(props.trackRow.url)
+const removeMultitrackItem = () => multitrackList.delete(props.trackRow.url)
+</script>
+
+<template>
+  <div class="flex flex-col justify-between flex-1 items-end *:rounded-sm">
+    <UButton :disabled="isBatchRunning" size="icon" @click="removeMultitrackItem">
+      <Icon :name="ICON__DOWNLOAD" />
+    </UButton>
+
+    <UButton
+      v-if="inMultitrackList"
+      :disabled="isBatchRunning"
+      size="icon"
+      @click="removeMultitrackItem"
+    >
+      <Icon :name="ICON__TRASH" />
+    </UButton>
+    <UButton v-else size="icon" :disabled="isBatchRunning" @click="addMultitrackItem">
+      <Icon :name="ICON__PLUS" />
+    </UButton>
+  </div>
+</template>
