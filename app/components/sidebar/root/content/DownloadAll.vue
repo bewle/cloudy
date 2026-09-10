f843594cb600beb93e789a5a2bc9041d2b4ce014
bewle <everbelle@proton.me>
2026-09-10 15:46:25 -0400

    fix: improve guards + delete download on complete

--- app/components/sidebar/root/content/DownloadAll.vue
+++ app/components/sidebar/root/content/DownloadAll.vue
@@ -6,7 +6,16 @@ const { sources } = injectSidebarRootContentContext()
 const { tab } = useSidebarState()
 const { downloadBatch, isBatchRunning } = useDownloads()
 
+const hasValidTracks = computed(() => {
+  if (tab.value !== 'multitrack') return false
+  if (!sources.multitrack.items.value.length) return false
+
+  return sources.multitrack.items.value.some(row => row.status === 'ready')
+})
+
 const handleDownloadAll = () => {
+  if (hasValidTracks.value) return
+
   if (!tab.value || !['artist', 'playlist', 'multitrack'].includes(tab.value)) return
 
   const rows = sources[tab.value].items.value
@@ -21,7 +30,13 @@ const handleDownloadAll = () => {
 </script>
 
 <template>
-  <UButton class="shrink-0" size="icon" :is-loading="isBatchRunning" @click="handleDownloadAll">
+  <UButton
+    :disabled="!hasValidTracks"
+    class="shrink-0"
+    size="icon"
+    :is-loading="isBatchRunning"
+    @click="handleDownloadAll"
+  >
     <Icon :name="ICON__DOWNLOAD" />
   </UButton>
 </template>
--- app/components/sidebar/root/content/list/CardButtons.vue
+++ app/components/sidebar/root/content/list/CardButtons.vue
@@ -3,24 +3,33 @@ const props = defineProps<{
   trackRow: TrackRow
 }>()
 
-const { downloadSingle, isBatchRunning } = useDownloads()
+const { downloadSingle, isBatchRunning, downloads } = useDownloads()
 
 const { multitrackList } = useSidebarState()
 const inMultitrackList = computed(() => multitrackList.has(props.trackRow.url))
+const isDownloading = computed(() => downloads.has(props.trackRow.url))
 
 const addMultitrackItem = () => multitrackList.add(props.trackRow.url)
 const removeMultitrackItem = () => multitrackList.delete(props.trackRow.url)
 
-const download = () =>
-  downloadSingle(props.trackRow.url, {
+const download = async () => {
+  if (props.trackRow.status !== 'ready') return
+  await downloadSingle(props.trackRow.url, {
     meta: props.trackRow.status === 'ready' ? props.trackRow.track : undefined,
     save: true,
   })
+
+  downloads.delete(props.trackRow.url)
+}
 </script>
 
 <template>
-  <div class="flex flex-1 flex-col items-end justify-between *:rounded-sm">
-    <UButton :disabled="isBatchRunning" size="icon" @click="download">
+  <div class="flex flex-col ml-auto items-end self-end justify-between shrink-0 *:rounded-sm">
+    <UButton
+      :disabled="isDownloading || isBatchRunning || trackRow.status !== 'ready'"
+      size="icon"
+      @click="download"
+    >
       <Icon :name="ICON__DOWNLOAD" />
     </UButton>
 
