dd58ea7c081dd334025f38ffab899f1806b337c3
bewle <everbelle@proton.me>
2026-09-14 15:41:16 -0400

    feat: add frontend utils

--- app/utils/file.ts
+++ app/utils/file.ts
@@ -1,3 +1,7 @@
 export function bufferToBlob(buffer: ArrayBuffer | ArrayBuffer[], type: string) {
   return new Blob(Array.isArray(buffer) ? buffer : [buffer], { type })
 }
+
+export function getZipFileName() {
+  return `cloudy-${new Date().toISOString()}.zip`
+}
