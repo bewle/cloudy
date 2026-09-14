1e0695fd8d54e12c00b87b0daf746d5e86b828de
bewle <everbelle@proton.me>
2026-09-14 15:42:01 -0400

    feat: add progress underlay component + refactor main input to use `useDownloads`

---
+++ app/components/u/ProgressUnderlay.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+export interface UProgressUnderlayProps {
+  class?: HTMLAttributes['class']
+  progress?: number
+}
+
+const props = withDefaults(defineProps<UProgressUnderlayProps>(), {
+  progress: 0,
+})
+</script>
+
+<template>
+  <div
+    :class="cn('bg-primary/10 h-full inset-0 absolute pointer-events-none', props.class)"
+    :style="{
+      width: `${progress * 100}%`,
+    }"
+  />
+</template>
