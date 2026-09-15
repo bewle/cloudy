129e53e1c21b4d4295bffddccb73de177083b1f0
bewle <everbelle@proton.me>
2026-09-14 20:39:01 -0400

    feat: add constants + general util

--- shared/constants/sidebar.ts
+++ shared/constants/sidebar.ts
@@ -47,6 +47,9 @@ export const SIDEBAR__BUTTON_META = {
 >
 
 export type SidebarButtonKey = (typeof SIDEBAR__BUTTON_KEYS)[number]
+export type SidebarTabKey = {
+  [K in SidebarButtonKey]: (typeof SIDEBAR__BUTTON_META)[K]['isTab'] extends true ? K : never
+}[SidebarButtonKey]
 export type SidebarTrackSourceKey = {
   [K in SidebarButtonKey]: (typeof SIDEBAR__BUTTON_META)[K]['isTrackSource'] extends true
     ? K
