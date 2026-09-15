88c2652eec10d1025e86e9ef9f6c04235cb9ee6a
bewle <everbelle@proton.me>
2026-09-15 16:13:29 -0400

    feat: enhance sidebar card styles

--- app/components/sidebar/root/content/list/Card.vue
+++ app/components/sidebar/root/content/list/Card.vue
@@ -5,7 +5,11 @@ const props = defineProps<{ class?: HTMLAttributes['class'] }>()
 </script>
 
 <template>
-  <div :class="cn('p-3 border border-border rounded bg-surface flex gap-3', props.class)">
+  <div
+    :class="
+      cn('h-13 py-2 px-2.5 items-center flex items-center bg-surface flex-row gap-2', props.class)
+    "
+  >
     <slot />
   </div>
 </template>
