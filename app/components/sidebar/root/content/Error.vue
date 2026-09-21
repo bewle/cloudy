66f1fb88988fe5f5924f8db82971795f5bb254bb
bewle <everbelle@proton.me>
2026-09-21 16:17:50 -0400

    refactor: use `useParsedError`

--- app/components/sidebar/root/content/Error.vue
+++ app/components/sidebar/root/content/Error.vue
@@ -3,10 +3,10 @@ const { error } = defineProps<{
   error: Error | undefined
 }>()
 
-const parsedError = computed(() => parseError(error))
+const parsedError = useParsedError(() => error)
 
 const icon = computed(() => {
-  switch (parsedError.value.status) {
+  switch (parsedError.value?.status) {
     case 404:
       return ICON__404
     default:
@@ -19,10 +19,10 @@ const icon = computed(() => {
   <UStateRoot>
     <UStateIcon :name="icon" />
     <UStateTitle>
-      {{ parsedError.message }}
+      {{ parsedError?.message }}
     </UStateTitle>
-    <UStateDescription v-if="parsedError.why">
-      {{ parsedError.why }}
+    <UStateDescription v-if="parsedError?.why">
+      {{ parsedError?.why }}
     </UStateDescription>
   </UStateRoot>
 </template>
