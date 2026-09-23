a7ce14914d4480e07f7ad2a13ff00890f1ca0150
bewle <everbelle@proton.me>
2026-09-23 17:25:28 -0400

    feat: update button variants + add soft

--- app/components/u/Button.vue
+++ app/components/u/Button.vue
@@ -18,13 +18,13 @@ export const buttonStyles = tv({
     },
     variant: {
       'default':
-        // 'text-muted-foreground not-[[data-state=active],[data-state=on]]:hover:(bg-background-hover text-foreground) active:(bg-background-active text-foreground) focus-visible:bg-background-hover data-[state=active]:(bg-background-active text-foreground) data-[state=on]:(bg-background-active text-foreground)',
-        'text-muted-foreground not-[[data-state=active],[data-state=on]]:hover:(bg-background-hover text-foreground) focus-visible:bg-background-hover data-[state=active]:(bg-background-active text-foreground) data-[state=on]:(bg-background-active text-foreground)',
+        'text-muted-foreground not-reka-active:hover:(bg-background-hover text-foreground) focus-visible:bg-background-hover reka-active:(bg-background-active text-foreground)',
+      'soft':
+        'text-muted-foreground bg-surface border border-border not-reka-active:hover:(bg-surface-hover border-border-strong text-foreground border-border-strong) focus-visible:bg-surface-hover reka-active:(bg-surface-active border-border-strong-active text-foreground)',
       'ghost-danger':
-        'text-danger-foreground not-[[data-state=active],[data-state=on]]:hover:(bg-danger-hover text-foreground) active:(bg-danger-active text-foreground) focus-visible:bg-danger-hover',
+        'text-danger-foreground not-reka-active:hover:(bg-danger-hover text-foreground) active:(bg-danger-active text-foreground) focus-visible:bg-danger-hover',
       'outline':
-        // 'bg-surface not-[[data-state=active],[data-state=on]]:hover:(bg-surface-hover border-border-hover) active:(bg-surface-active border-border-active) focus-visible:bg-surface-hover border border-border data-[state=active]:(bg-surface-active border-border-active) data-[state=on]:(bg-surface-active border-border-active)',
-        'bg-surface not-[[data-state=active],[data-state=on]]:hover:(bg-surface-hover border-border-hover) focus-visible:bg-surface-hover border border-border data-[state=active]:(bg-surface-active border-border-active) data-[state=on]:(bg-surface-active border-border-active)',
+        'bg-surface not-reka-active:hover:(bg-surface-hover border-border-hover) focus-visible:bg-surface-hover border border-border reka-active:(bg-surface-active border-border-active)',
     },
   },
 })
